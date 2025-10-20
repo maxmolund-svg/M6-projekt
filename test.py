421965a2e1c60f630379f13b4485f98207c482eb
tunil21 <tunil@icloud.com>
2025-10-20 10:58:38 +0200

    size input change

--- test.py
+++ test.py
@@ -7,5 +7,6 @@ def rng(size,mines):
     print("Hello world")
 
 def main():
-    s=input("What size map would you like to play?: ")
+    s=input("What size map would you like to play?(s,m or l): ")
     m=input("How many mines would you like?: ")
+    rng(s,m)
