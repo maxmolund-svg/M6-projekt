b982b9ceaa2946ba20cb01229ea5da3361ee4caa
Max Molund <maxmolund@Maxs-MacBook-Pro.local>
2025-10-20 11:04:28 +0200

    start of rng func

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from random import randint
 
 squares={"mines":[],"noMines":[]}
 
-def rng(a, b):
-    size = a*a
-    for i in range (1, b-1):
+def rng(size, mines):
+    
+    for i in range (1, mines-1):
         mine = randint(0, size)
         
