42b5cf2fadb845d1d5f021283e59102f6543bdc3
Max Molund <maxmolund@Maxs-MacBook-Pro.local>
2025-10-20 11:10:20 +0200

    map size

--- test.py
+++ test.py
@@ -7,9 +7,16 @@ squares={"mines":[],"noMines":[]}
 
 
 def rng(size, mines):
+
+    if size == "s":
+        map = 5*5
+    elif size == "m":
+        map = 6*6
+    else: map == 7*7
     
+
     for i in range (1, mines-1):
-        mine = randint(0, size)
+        mine = randint(0, map)
         
 
 
