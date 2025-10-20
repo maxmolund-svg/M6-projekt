90bf798cc835a18656953fd0208d6b09a940890d
Max Molund <maxmolund@Maxs-MacBook-Pro.local>
2025-10-20 10:55:21 +0200

    start of rng func

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+from random import randint
+
 #minesweeper M6
 
 squares={"mines":[],"noMines":[]}
 
-def rng():
-    print("Hello world")
+def rng(a, b):
+    size = a*a
+    for i in range (1, size-1):
+        mine = randint(0, size)
+        
