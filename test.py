6b3ff6a375fe2b194d273f8f87cc011a257bcd91
tunil21 <tunil@icloud.com>
2025-10-22 15:19:44 +0200

    changed representation to includ coordinates, bool for if flagged or dug, and hopefully amount of adjacent mines

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from random import randint
 import random
+import math
 #minesweeper M6
 
 
@@ -12,8 +13,44 @@ def rng(mines):
     for mine in squares["mines"]:
         squares["noMines"].remove(mine)
 
+def findAdjacent():
+    for i in range(len(squares["noMines"])):
+        x1=squares["noMines"][i][1][0]
+        y1=squares["noMines"][i][1][1]
+        adjacent=0
+        for j in range(len(squares["mines"])):
+            x2=squares["mines"][j][1][0]
+            y2=squares["mines"][j][1][1]
+            if x2==(x1-1) and y2==(y1-1):
+                adjacent+=1
+            elif x2==(x1-1) and y2==(y1):
+                adjacent+=1
+            elif x2==(x1-1) and y2==(y1+1):
+                adjacent+=1   
+            elif x2==(x1) and y2==(y1-1):
+                adjacent+=1
+            #elif x2==(x1) and y2==(y1): samma kan ej vara närliggande
+            #    adjacent+=1
+            elif x2==(x1) and y2==(y1+1):
+                adjacent+=1
+            elif x2==(x1+1) and y2==(y1-1):
+                adjacent+=1
+            elif x2==(x1+1) and y2==(y1):
+                adjacent+=1
+            elif x2==(x1+1) and y2==(y1+1):
+                adjacent+=1
+        squares["noMines"][i].append(adjacent)
 
 
+def testPrint(size): #trasig
+    for x in range(size):
+        width=math.isqrt(size)
+        if x!=0 and math.floor(x/width)>math.floor((x-1)/width):
+            print('\n',end="") 
+        elif x==squares["mines"][x][0]:
+            print("m",end="")
+        elif x==squares["noMines"][x][0]:
+            print(f"{squares['noMines'][x][4]}",end="")
 
 def main():
     s=input("What size map would you like to play?(s,m or l): ")
@@ -25,13 +62,26 @@ def main():
     else: 
         size = 7*7
 
-    squares["noMines"] = list(range(size))
+    for n in range(size):
+        width=math.isqrt(size)
+        
+        x=n%width
+        
+        if n==0:
+            y=0 #solves divide by 0 error
+        else:
+            y=math.floor(n/width)
+
+        squares["noMines"].append([n,(x,y),False,False])
+        #print((x,y))
 
     m=int(input("How many mines would you like?: "))
 
     rng(m)
-
-    print(squares)
-
+    
+    findAdjacent()
+    
+    testPrint(size)
+    #print(squares)
 
 main()
